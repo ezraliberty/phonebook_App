92ff677d8c9afbaccf21e7e34b8ee72d57b52d52
Thomas Ezra <32423962+ezraliberty@users.noreply.github.com>
2024-10-23 21:26:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,34 +31,6 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-//   {
-//     id: 5,
-//     name: "Lord Lugard",
-//     number: "39-23-56782",
-//   },
-// ];
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => response.json(persons));
 });
